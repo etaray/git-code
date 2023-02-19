2b1cef019180a20023234c19c3f13cd7de3c993a
Raymond <ereta19@yahoo.com>
2023-02-19 14:17:42 +0100

    Updated DBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,12 @@
+
+Raymond DBConnect STARTS
+New chat
+Push to GitHub platforms.
+Push Error Solution.
+Delete GitHub repository.
+Shell Definition
+Monitor Server Every Minute.
+While Loop Explanation
+The Lord is my sheperd
+Raymond DBConnect ENDS
+
